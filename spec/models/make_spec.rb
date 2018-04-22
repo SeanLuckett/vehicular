9b26dfb042bfb34c0a63ce6ee5da88b9241c5b70
Sean Luckett <whitecrane007@gmail.com>
2018-04-22 10:08:18 -0600

    Changes Make's name uniqueness constraint to be case-insensitive

--- spec/models/make_spec.rb
+++ spec/models/make_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Make, type: :model do
 
   it 'is invalid if name is taken' do
     existing_make = create :make
-    bad_make = build :make, name: existing_make.name
+    bad_make = build :make, name: existing_make.name.downcase
     bad_make.valid?
     expect(bad_make.errors[:name]).to include 'has already been taken'
   end
